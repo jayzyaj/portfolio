6ec48ba56dbc7a1b64a94ce491a936519370083e
John Clifford de Vera <john.devera@circleyy.com>
2020-05-23 16:04:52 +0800

    Updates

---
+++ precache-manifest.1c4f03839201badf842f99fdcc0fef40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c5b06e62d06794bd9a9ef9ecefc1923",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "977ca1ebf42f7458d08d",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ef9f8e918c9b3c9d7381",
+    "url": "/portfolio/static/js/2.dc38c232.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.dc38c232.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "977ca1ebf42f7458d08d",
+    "url": "/portfolio/static/js/main.f5810c9e.chunk.js"
+  },
+  {
+    "revision": "e86dc54f53df0d7ffe34",
+    "url": "/portfolio/static/js/runtime-main.acfa1960.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
