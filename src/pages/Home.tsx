08b0947e22256aaa5e665f451f55740a5c267d6d
John Clifford de Vera <john.devera@circleyy.com>
2020-05-28 20:57:08 +0800

    Added css animation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,38 +1,41 @@
 import React from 'react';
 import '../App.css';
-import { colors } from '../constants/themes/colors';
 import Lottie from 'react-lottie';
+import { colors } from '../constants/themes/colors';
 import earth from '../assets/animations/hello-earth.json';
 
 const defaultOptions = {
   loop: false,
-  autoplay: true, 
+  autoplay: true,
   animationData: earth,
   rendererSettings: {
-    preserveAspectRatio: 'xMidYMid slice'
-  }
+    preserveAspectRatio: 'xMidYMid slice',
+  },
 };
 
 function Home() {
   return (
     <div className="App">
-      <Lottie options={defaultOptions}
+      <div className="App-logo">
+        <Lottie
+          options={defaultOptions}
           style={{ backgroundColor: 'transparent' }}
           height={400}
           width={400}
         />
-        <p style={{ color: colors.primary, fontWeight: 'bolder', fontSize: 34 }}>
-          Hi! I'm John Clifford de Vera
-        </p>
-        <a
-          className="App-link"
-          href="https://www.linkedin.com/in/john-clifford-de-vera-511150159/"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ color: colors.tertiary }}
-        >
-          Linkedin Profile
-        </a>
+      </div>
+      <p style={{ color: colors.primary, fontWeight: 'bolder', fontSize: 34 }}>
+        Hi! I'm John Clifford de Vera
+      </p>
+      <a
+        className="App-link"
+        href="https://www.linkedin.com/in/john-clifford-de-vera-511150159/"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ color: colors.tertiary }}
+      >
+        Linkedin Profile
+      </a>
     </div>
   );
 }
